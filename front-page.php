c216df2eb365089aae501dff779d4188a9248b3e
Chris Schmieder <ckschmieder@gmail.com>
2016-02-10 04:15:35 -0500

    updates for front page development

--- front-page.php
+++ front-page.php
@@ -13,12 +13,7 @@ global $more;		// Should WP display the conent after ---more--- ? (0=false; 1=tr
 
 <!-- Jumbotron -->
 		<section id="jumbotron" class="lander-section">
-			<div class="indent clear">
-				<div class="row">
-					<div class="col-lg-4">Column</div>
-					<div class="col-lg-4">Column</div>
-					<div class="col-lg-4">Column</div>
-				</div>
+			<div class="indent clear">				
 				<?php 
 				$query = new WP_Query( 'pagename=jumbotron' );
 				// The Loop
