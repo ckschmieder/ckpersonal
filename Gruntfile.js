f1a564f29a86e3383a865a7d93bb1f216be25fe3
Chris Schmieder <ckschmieder@gmail.com>
2016-02-25 22:58:20 -0500

    added typed.js to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,8 @@ module.exports = function (grunt) {
 
 					"js/script.min.js": [
 						"node_modules/jquery/dist/jquery.js",
-						"node_modules/bootstrap-sass/assets/javascripts/bootstrap.js"
+						"node_modules/bootstrap-sass/assets/javascripts/bootstrap.js",
+						"js/typed.js"
 						
 
 
