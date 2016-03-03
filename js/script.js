ecd8882598321d1d85161d07dbfe200d818743e8
Chris Schmieder <ckschmieder@gmail.com>
2016-03-03 04:25:19 -0500

    Style updates for navigation

--- js/script.js
+++ js/script.js
@@ -13,6 +13,22 @@ $(function() {
         $('.fullheight').css('height', wheight); //set to window tallness
     });
 
+    //Use smooth scrolling when clicking on navigation
+    $('.main-navigation a[href*=#]').click(function() {
+      if (location.pathname.replace(/^\//, '') ===
+        this.pathname.replace(/^\//, '') &&
+        location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - topoffset + 2
+          }, 500);
+          return false;
+        } //target.length
+      } //click function
+    }); //smooth scrolling
+
 
 });
 
